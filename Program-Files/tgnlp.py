d3065b6cb2ab35a50fc0b3556e47fed22d936868
Ankit Kumar Ojha <ankitkumarojha2@gmail.com>
2016-09-12 15:01:39 +0530

    Added few more comments
    These comments would improve the readability of the code

--- Program-Files/tgnlp.py
+++ Program-Files/tgnlp.py
@@ -7,19 +7,19 @@ from replacepattern import *            # Replaces short forms like 't, 'd to no
 from repeatchar import *                # Corrects the Repeated Characters in a word like "Yeaaaaah!" becomes "yeah"
 
 def processNLQuery(nlq):
-	f=open("big.txt",'a')                 #
-	lemmatizer=WordNetLemmatizer()        #
-	stemmer=PorterStemmer()               #
-	nlq=replace(nlq)                      #
-	words=word_tokenize(nlq)              #
-	lwr=[word.lower() for word in words]  # 	
-	rpchar=[repchar(word) for word in lwr] #repeated characters
-	splchk=[correct(word) for word in rpchar] #spelling check
-	stopw=set(stopwords.words('english')) #extracting stop words	
-	filteredstopwords=[word for word in splchk if word not in stopw] #removing stop words
-	#stemmed=[stemmer.stem(word) for word in filteredstopwords] #
-	lemmatized=[lemmatizer.lemmatize(word) for word in filteredstopwords] #	
-	for word in lemmatized:               #
-		f.write(word + " ")                 #
-	f.close()                             #
-	return lemmatized;                    #
+	lemmatizer=WordNetLemmatizer()        # initializing lemmatizer from WordNet
+	stemmer=PorterStemmer()               # initializing stemmer from PorterStemmer
+	nlq=replace(nlq)                      # Replacing all short-form words like 't,'d,'m to not,would,am etc.
+	words=word_tokenize(nlq)              # Separating words from the NLQuery and storing it in a list
+	lwr=[word.lower() for word in words]  # Converting all letters to lower case to uniformity	
+	rpchar=[repchar(word) for word in lwr] # Corrects repeated characters in a word (yeaaaaaaah! to yeah!)
+	splchk=[correct(word) for word in rpchar] # Checking spellings of the words and correcting them if necessary using a wordbase(big.txt)
+	stopw=set(stopwords.words('english')) # extracting stop words(Words those are not relevant in this context ex: is,the,a,an..
+	filteredstopwords=[word for word in splchk if word not in stopw] # removing stop words from the NLQuery
+	#stemmed=[stemmer.stem(word) for word in filteredstopwords] # Stemming the words
+	lemmatized=[lemmatizer.lemmatize(word) for word in filteredstopwords] #	Converting the words to root form (feet-> foot, cars->car)
+	f=open("big.txt",'a')                 # Opening file which is our wordbase, this is used in spellcheck. Words are appended so that spellcheck algorithm can be improved which works on probability on # of occurrence of words.
+	for word in lemmatized:                
+		f.write(word + " ")           #	writing words in the wordbase
+	f.close()                             # closing file (wordbase)
+	return lemmatized;                    # returning the Processed Query.
